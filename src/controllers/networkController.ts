085984ecdeb1d3b18dfea200e5d97052a46b7986
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2023-05-09 13:11:39 -0300

    feat: get and delete networks

--- src/controllers/networkController.ts
+++ src/controllers/networkController.ts
@@ -1,30 +1,44 @@
 import { AuthenticatedRequest } from "../middlewares/authenticationMiddleware.js";
 import { Response } from "express";
 import { networkSchema } from "../schemas/networkSchema.js";
-import { newNetwork } from "../services/networkService.js";
+import { newNetwork, viewNetworks, deleteNet } from "../services/networkService.js";
 import { Prisma } from "@prisma/client";
 
-async function createNetwork(req:AuthenticatedRequest,res:Response) {
+async function createNetwork(req: AuthenticatedRequest, res: Response) {
     const network = req.body as Prisma.NetworkCreateInput;
     const user = req.userId as number;
     try {
         const validation = networkSchema.validate(req.body)
-        if(validation.error) throw new Error (`${validation.error.details}`)
+        if (validation.error) throw new Error(`${validation.error.details}`)
         await newNetwork(network, user)
 
         res.sendStatus(201)
 
     } catch (error) {
         res.status(422).send(error.message)
-    }  
+    }
 }
 
-async function getNetworks(req:AuthenticatedRequest,res:Response) {
-    
+async function getNetworks(req: AuthenticatedRequest, res: Response) {
+    const userId = req.userId as number;
+    const { id } = req.query;
+    try {
+        const wifi = await viewNetworks(userId, Number(id))
+        res.status(200).send(wifi)
+    } catch (error) {
+        res.status(422).send(error.message)
+    }
 }
 
-async function deleteSomeNetwork(req:AuthenticatedRequest,res:Response) {
-    
+async function deleteSomeNetwork(req: AuthenticatedRequest, res: Response) {
+    const {id} = req.params;
+    const userId = req.userId as number;
+    try {
+        await deleteNet(Number(id), userId)
+        res.sendStatus(200)
+    } catch (error) {
+        res.status(422).send(error.message)
+    }
 }
 
 export { createNetwork, getNetworks, deleteSomeNetwork }
--- src/services/networkService.ts
+++ src/services/networkService.ts
@@ -1,16 +1,39 @@
-import { createNetwork } from "../repositories/networkRepository.js";
+import { createNetwork, deleteNetworkById, findNetworkById, findUserNetworks } from "../repositories/networkRepository.js";
 import { Prisma } from "@prisma/client";
 import Cryptr from "cryptr";
+import httpStatus from "http-status";
+import { number } from "joi";
 const cryptr = new Cryptr("Secret");
 
-async function newNetwork(data:Prisma.NetworkCreateInput, userId:number){
+async function newNetwork(data: Prisma.NetworkCreateInput, userId: number) {
 
     const hashPassword = cryptr.encrypt(data.password)
 
     const networkData = { ...data, password: hashPassword, userId }
 
-    await createNetwork({...networkData})
+    await createNetwork({ ...networkData })
+}
+async function viewNetworks(userId: number, id: number) {
+    if (id) {
+        const wifi = await findNetworkById(id)
+        if (wifi.userId != userId) throw httpStatus.UNAUTHORIZED
+        const decryptedPassword = cryptr.decrypt(wifi.password)
+        return { ...wifi, password: decryptedPassword }
+    } else {
+        const wifis = await findUserNetworks(userId)
+        wifis.forEach(wifi => {
+            const decryptedPassword = cryptr.decrypt(wifi.password)
+            wifi.password = decryptedPassword
+        })
+        return wifis
+    }
 }
 
+async function deleteNet(id:number, userId:number) {
+    const wifi = await findNetworkById(id)
+    if(wifi.userId != userId) throw httpStatus.UNAUTHORIZED
 
-export { newNetwork}
+    await deleteNetworkById(id)
+}
+
+export { newNetwork,viewNetworks, deleteNet }
